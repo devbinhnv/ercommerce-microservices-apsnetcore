df6a1f8ec13aaf72996ba61d654239627c3619ec
Lander Nguyen <work.beros@gmail.com>
2025-06-03 16:57:48 +0700

    feat: create common logger config

--- src/BuildingBlocks/Common.Logging/SeriLogger.cs
+++ src/BuildingBlocks/Common.Logging/SeriLogger.cs
@@ -1,7 +1,26 @@
-﻿namespace Common.Logging
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+
+namespace Common.Logging
 {
-    public class SeriLogger
+    public static class SeriLogger
     {
-        
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
+            (context, configuration) =>
+            {
+                var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-");
+                var environmentName = context.HostingEnvironment.EnvironmentName ?? "Development";
+                configuration
+                    .WriteTo.Debug()
+                    .WriteTo.Console(outputTemplate:
+                        "[{Timestamp:HH:mm:ss} {Level} {SourceContext}{NewLine}{Message:lgj}{NewLine}-{Exception}{NewLine}]")
+                    .Enrich.FromLogContext()
+                    //Need: install package Serilog.Enrichers;
+                    .Enrich.WithMachineName()
+                    .Enrich.WithProperty("ApplicationName", applicationName)
+                    .Enrich.WithProperty("EnvironmentName", environmentName)
+                    .ReadFrom.Configuration(context.Configuration);
+            };
     }
 }
