0c6905f76b70a358cab59b9cb6b15d29eb2e64b4
Lander Nguyen <work.beros@gmail.com>
2025-06-17 01:13:31 +0700

    feat: create DbContext, entity and seed data for Product.API

---
+++ src/Services/Product.API/Persistence/ProductContext.cs
@@ -0,0 +1,45 @@
+﻿using Contracts.Domains.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Product.API.Entities;
+
+namespace Product.API.Persistence;
+
+public class ProductContext : DbContext
+{
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+    {
+    }
+    public DbSet<CatalogProduct> Products { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var modifiedEntryEntites = ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Modified
+                    || e.State == EntityState.Added
+                    || e.State == EntityState.Deleted);
+
+        foreach (var item in modifiedEntryEntites)
+        {
+            switch (item.State)
+            {
+                case EntityState.Added:
+                    if (item.Entity is IDateTracking addedEntity)
+                    {
+                        addedEntity.CreatedDate = DateTimeOffset.UtcNow;
+                        //item.State = EntityState.Added;
+                    }
+                    break;
+
+                case EntityState.Modified:
+                    Entry(item.Entity).Property("Id").IsModified = false;
+                    if (item.Entity is IDateTracking modifiedEntity)
+                    {
+                        modifiedEntity.LastModifiedDate = DateTimeOffset.UtcNow;
+                        //item.State = EntityState.Modified;
+                    }
+                    break;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
