0abe5c2a39787069710128435ff51aa9e5c8293c
Lander Nguyen <work.beros@gmail.com>
2025-06-25 01:10:49 +0700

    feat: define CustomerContext, entity and Migrations

---
+++ src/Services/Customer.API/Persistence/CustomerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Customer.API.Persistence;
+
+public class CustomerContext : DbContext
+{
+    public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Entities.Customer> Customers { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Entities.Customer>(entity =>
+        {
+            entity.HasIndex(e => e.Id).IsUnique();
+            entity.HasIndex(e => e.EmailAddress).IsUnique();
+        });
+    }
+}
