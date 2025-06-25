b07d3ca6eb7b961b1b39b68089133ba61cce3a8b
Lander Nguyen <work.beros@gmail.com>
2025-06-26 01:19:46 +0700

    feat: define customer dto

---
+++ src/BuildingBlocks/Shared/DTOs/Customer/CreateOrUpdateCustomerDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.DTOs.Customer;
+
+public abstract class CreateOrUpdateCustomerDto
+{
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? EmailAddress { get; set; }
+}
+
+public class  CreateCustomerDto : CreateOrUpdateCustomerDto
+{
+    public string UserName { get; set; }
+}
+
+public class  UpdateCustomerDto : CreateOrUpdateCustomerDto
+{
+    
+}
