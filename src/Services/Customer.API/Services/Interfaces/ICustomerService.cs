aa102da2144d8a15d438329d5e0701951eee4824
Lander Nguyen <work.beros@gmail.com>
2025-06-26 01:18:56 +0700

    feat: complete customer service

--- src/Services/Customer.API/Services/Interfaces/ICustomerService.cs
+++ src/Services/Customer.API/Services/Interfaces/ICustomerService.cs
@@ -1,7 +1,12 @@
-﻿namespace Customer.API.Services.Interfaces;
+﻿using Shared.DTOs.Customer;
+
+namespace Customer.API.Services.Interfaces;
 
 public interface ICustomerService
 {
-    Task<IResult> GetCustomerByUserNameAsync(string userName);
     Task<IResult> GetCustomersAsync();
+    Task<IResult> GetCustomerByUserNameAsync(string customerName);
+    Task<IResult> CreateCustomerAsync(CreateCustomerDto createCustomerDto);
+    Task<IResult> UpdateCustomerAsync(int id, UpdateCustomerDto updateCustomerDto);
+    Task<IResult> DeleteCustomerAsync(int id);
 }
