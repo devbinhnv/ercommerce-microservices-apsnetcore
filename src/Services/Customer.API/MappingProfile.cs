bd53f129ba54e6cb57479e9c742db3407ab6ac5a
Lander Nguyen <work.beros@gmail.com>
2025-06-26 01:33:51 +0700

    feat : define MappingProfile

---
+++ src/Services/Customer.API/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Customer.API.Entities;
+using Infrastructure.Mappings;
+using Shared.DTOs.Customer;
+
+namespace Customer.API;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CustomerEntity, CustomerDto>();
+        CreateMap<CreateCustomerDto, CustomerEntity>();
+        CreateMap<UpdateCustomerDto, CustomerEntity>()
+            .IgnoreAllNonExisting() //[Extension method]: Ignore fields non existing when map
+            .IgnoreNullProperties(); //[Extension method]: Ignore fields that has null value
+    }
+}
