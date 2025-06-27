222fdeaf2b23f8d6856536805be3c115f4dfff7d
Lander Nguyen <work.beros@gmail.com>
2025-06-27 13:31:37 +0700

    feat: add logs to repository

--- src/Services/Basket.API/Repositories/BasketRepository.cs
+++ src/Services/Basket.API/Repositories/BasketRepository.cs
@@ -14,28 +14,34 @@ public class BasketRepository(
 
     public async Task<Cart?> GetBasketByUserName(string userName)
     {
+        logger.Information($"START: GetBasketByUserName for username {userName}");
         var basket = await redisCacheService.GetStringAsync(userName);
-        return string.IsNullOrEmpty(basket) ? null : 
-            serializeService.Deserialize<Cart>(basket);
+        logger.Information($"END: GetBasketByUserName for username {userName}");
+
+        return string.IsNullOrEmpty(basket) ? null : serializeService.Deserialize<Cart>(basket);
     }
 
     public async Task<bool> DeleteBasketFromUserName(string userName)
     {
         try
         {
+            logger.Information($"START: DeleteBasketFromUserName for username {userName}");
             await redisCacheService.RemoveAsync(userName);
+            logger.Information($"END: DeleteBasketFromUserName for username {userName}");
+
             return true;
         }
         catch(Exception e)
         {
-            logger.Error($"DeleteBasketFromUserName: ${e.Message}"); 
+            logger.Error($"Error DeleteBasketFromUserName: ${e.Message}"); 
             throw;
         }
     }
 
     public async Task<Cart> UpdateBasket(Cart cart, DistributedCacheEntryOptions? options = default)
     {
-        if(options is null)
+        logger.Information($"START: UpdateBasket for username {cart.UserName}");
+        if (options is null)
         {
             await redisCacheService.SetStringAsync(cart.UserName, serializeService.Seriallize(cart));
         }
@@ -43,6 +49,7 @@ public class BasketRepository(
         {
             await redisCacheService.SetStringAsync(cart.UserName, serializeService.Seriallize(cart), options);
         }
+        logger.Information($"END: UpdateBasket for username {cart.UserName}");
         return await GetBasketByUserName(cart.UserName);
     }
 }
