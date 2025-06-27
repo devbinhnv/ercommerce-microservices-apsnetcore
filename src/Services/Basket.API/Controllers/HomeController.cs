90bdb1fa2d8910656aab9c099b7cb3541e2b9d36
Lander Nguyen <work.beros@gmail.com>
2025-06-27 13:32:30 +0700

    feat: auto redirect home to swagger

---
+++ src/Services/Basket.API/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+[Route("/")]
+public class HomeController : ControllerBase
+{
+    //This controller help auto redirect to swagger
+    public IActionResult Index()
+    {
+        return Redirect("~/swagger");
+    }
+}
