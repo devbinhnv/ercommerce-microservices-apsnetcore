46ac54850049075ccad78f02a78257b478d4a4d1
Lander Nguyen <work.beros@gmail.com>
2025-06-27 14:07:08 +0700

    fix: validate CartItem

--- src/Services/Basket.API/Entities/CartItem.cs
+++ src/Services/Basket.API/Entities/CartItem.cs
@@ -1,12 +1,20 @@
-﻿namespace Basket.API.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basket.API.Entities;
 
 public class CartItem
 {
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "The field {0} should greater than 1")]
     public int Quantity { get; set; }
 
+    [Required]
+    [Range(0.1, double.PositiveInfinity, ErrorMessage = "The field {0} should greater than 1")]
     public decimal ItemPrice { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
     public string ItemNo { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
     public string ItemName { get; set; }
 }
