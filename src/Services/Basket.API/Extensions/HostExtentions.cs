775c902fec886a66b29a5c5044bdf336df83160d
Lander Nguyen <work.beros@gmail.com>
2025-06-27 10:23:21 +0700

    feat: define host config

---
+++ src/Services/Basket.API/Extensions/HostExtentions.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace Basket.API.Extensions;
+
+public static class HostExtentions
+{
+    public static void AddAppConfiguration(this WebApplicationBuilder builder)
+    {
+        var env = builder.Environment.EnvironmentName;
+        builder.Configuration
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env}.json", optional: false, reloadOnChange: true)
+               .AddEnvironmentVariables();
+    }
+}
