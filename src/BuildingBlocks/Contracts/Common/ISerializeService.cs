701a460de1dbc82680015d23ca1f0c645b6f760f
Lander Nguyen <work.beros@gmail.com>
2025-06-27 12:38:09 +0700

    feat: define serialize service

---
+++ src/BuildingBlocks/Contracts/Common/ISerializeService.cs
@@ -0,0 +1,8 @@
+﻿namespace Contracts.Common;
+
+public interface ISerializeService
+{
+    string? Seriallize<T>(T obj);
+    string Seriallize<T>(T obj, Type type);
+    T Deserialize<T>(string text);
+}
