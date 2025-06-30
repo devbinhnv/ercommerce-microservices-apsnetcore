2addf1f7a1d467014c6cbacced3988d7899f4117
Lander Nguyen <work.beros@gmail.com>
2025-06-30 13:46:27 +0700

    feat: define domain exceptions

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Exceptions;
+
+public class EntityNotFoundException : ApplicationException
+{
+    public EntityNotFoundException()
+    {
+
+    }
+
+    public EntityNotFoundException(string message) : base(message)
+    {
+
+    }
+
+    public EntityNotFoundException(string entity, object key)
+        : base($"Entity \"{entity}\" {key} was not found.")
+    {
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/InvalidEntityTypeException.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Exceptions;
+
+public class InvalidEntityTypeException : ApplicationException
+{
+    public InvalidEntityTypeException()
+    {
+        
+    }
+
+    public InvalidEntityTypeException(string message) : base(message)
+    {
+        
+    }
+
+    public InvalidEntityTypeException(string entity, object type)
+        : base($"Entity \"{entity}\" not supported type: {type}.")
+    {
+        
+    }
+}
