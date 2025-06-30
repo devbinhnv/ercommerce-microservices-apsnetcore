66fb5bd5d1ed23829ea313bf526eae356a8abac9
Lander Nguyen <work.beros@gmail.com>
2025-06-30 13:19:51 +0700

    feat: define OrderContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/Migrations/20250630045356_Order-Username-Ser-nvarchar150.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderUsernameSernvarchar150 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Order",
+                type: "nvarchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)");
+        }
+    }
+}
