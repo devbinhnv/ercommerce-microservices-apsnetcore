9ae38d9aebebc2208ac6a02f8c75c85beadec15b
Lander Nguyen <work.beros@gmail.com>
2025-06-30 13:18:58 +0700

    fear: define order entity

---
+++ src/Services/Ordering/Ordering.Domain/Entities/OrderEntity.cs
@@ -0,0 +1,36 @@
+﻿using Contracts.Domains;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ordering.Domain.Entities;
+
+[Table("Order")]
+public class OrderEntity : EntityAuditBase<long>
+{
+    [Required]
+    [Column(TypeName = "nvarchar(150)")]
+    public string UserName { get; set; }
+
+    [Column(TypeName ="decimal(10,2)")]
+    public decimal TotalPrice { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(250)")]
+    public string LastName { get; set; }
+
+    [Required]
+    [Column(TypeName ="nvarchar(250)")]
+    public string EmailAddress { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(max)")]
+    public string ShippingAddress { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(max)")]
+    public string InvoiceAddress { get; set; }
+}
