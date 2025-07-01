b1064f202657ff175bc1721d59da96067306b4b7
devbinhnv <dev.binhnv@gmail.com>
2025-07-01 15:07:20 +0700

    refactor: .

--- src/BuildingBlocks/Contracts/Common/IRepositoryBaseAsync.cs
+++ src/BuildingBlocks/Contracts/Common/IRepositoryBaseAsync.cs
@@ -5,29 +5,11 @@ using System.Collections;
 
 namespace Contracts.Common;
 
-public interface IRepositoryBaseAsync<TEntity, TKey, TContext> : IRepositoryQueryBase<TEntity, TKey, TContext>
+public interface IRepositoryBaseAsync<TEntity, TKey, TContext> : IRepositoryBaseAsync<TEntity, TKey>
     where TEntity : EntityBase<TKey>
     where TContext : DbContext
 {
-    Task<TKey> CreateAsync(TEntity entity);
-
-    Task<IList<TKey>> CreateListAsync(IEnumerable<TEntity> entities);
-
-    Task UpdateAsync(TEntity entity);
-
-    Task UpdateListAsync(IEnumerable<TEntity> entities);
-
-    Task DeleteAsync(TEntity entity);
-
-    Task DeleteListAsync(IEnumerable<TEntity> entities);
-
-    Task<IDbContextTransaction> BeginTransactionAsync();
-
-    Task EndTransactionAsync();
-
-    Task RollbackTransactionAsync();
-
-    Task SaveChangeAsync();
+    
 }
 
 public interface IRepositoryBaseAsync<TEntity, TKey> : IRepositoryQueryBase<TEntity, TKey>
--- src/BuildingBlocks/Contracts/Common/IRepositoryQueryBase.cs
+++ src/BuildingBlocks/Contracts/Common/IRepositoryQueryBase.cs
@@ -4,22 +4,11 @@ using System.Linq.Expressions;
 
 namespace Contracts.Common;
 
-public interface IRepositoryQueryBase<TEntity, TKey, TContext>
+public interface IRepositoryQueryBase<TEntity, TKey, TContext> : IRepositoryQueryBase<TEntity, TKey>
     where TEntity : EntityBase<TKey>
     where TContext : DbContext
 {
-    IQueryable<TEntity> FindAll(bool trackChanges = false);
 
-    IQueryable<TEntity> FindAll(bool trackChanges = false, params Expression<Func<TEntity, object>>[] includeProperties);
-
-    IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression, bool trackChanges = false);
-
-    IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression, bool trackChanges = false,
-        params Expression<Func<TEntity, object>>[] includeProperties);
-
-    Task<TEntity?> GetByIdAsync(TKey id);
-
-    Task<TEntity?> GetByIdAsync(TKey id, params Expression<Func<TEntity, object>>[] includeProperties);
 }
 
 public interface IRepositoryQueryBase<TEntity, TKey>
