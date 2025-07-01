8c9223834cf44f35e817fd509be29779d2f26ee5
Lander Nguyen <work.beros@gmail.com>
2025-07-01 08:56:30 +0700

    refactor: separate to smaller methods

--- src/Services/Ordering/Ordering.Application/ConfigServices.cs
+++ src/Services/Ordering/Ordering.Application/ConfigServices.cs
@@ -10,13 +10,24 @@ public static class ConfigServices
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddAutoMapper(Assembly.GetExecutingAssembly())
-                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
-                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
-                .AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>))
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
+                .AddMediatRBehaviors();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Add collections of behaviors that are used in the MediatR pipeline.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    private static IServiceCollection AddMediatRBehaviors(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>))
                 .AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>))
                 .AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
         return services;
     }
 }
