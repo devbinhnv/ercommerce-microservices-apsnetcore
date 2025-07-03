50b4570eabd1b56409875056fb28540c84fd112c
devbinhnv <dev.binhnv@gmail.com>
2025-07-03 18:35:45 +0700

    feat: define base  integration event

---
+++ src/BuildingBlocks/EvenBus/EvenBus.Messages/IntergrationBaseEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace EvenBus.Messages
+{
+    public record IntegrationBaseEvent() : IIntegrationEvent
+    {
+        public DateTime CreationDate { get; } = DateTime.UtcNow;
+        public Guid Id { get; set; }
+
+    }
+}
