ec4821a0245ce458d9beb666d296734dc9c71075
devbinhnv <dev.binhnv@gmail.com>
2025-07-10 10:01:11 +0700

    fix: remove total

--- src/Services/Basket.API/Entities/BasketCheckout.cs
+++ src/Services/Basket.API/Entities/BasketCheckout.cs
@@ -6,8 +6,6 @@ public class BasketCheckout
 {
     public string UserName { get; set; }
 
-    public decimal TotalPrice { get; set; }
-
     public string FirstName { get; set; }
 
     public string LastName { get; set; }
