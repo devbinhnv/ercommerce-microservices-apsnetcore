ed636f5c9832d0e10b48177133523601ab18c4cb
devbinhnv <dev.binhnv@gmail.com>
2025-07-12 14:04:39 +0700

    feat: create class for seed data

---
+++ src/Services/Inventory.Product.API/Persistence/InventoryDbSeed.cs
@@ -0,0 +1,43 @@
+﻿using Inventory.Product.API.Entities;
+using Inventory.Product.API.Extensions;
+using MongoDB.Driver;
+
+namespace Inventory.Product.API.Persistence;
+
+public class InventoryDbSeed
+{
+    public async Task SeedDataAsync(IMongoClient mongoClient, DatabaseSettings settings)
+    {
+        var databaseName = settings.DatabaseName;
+        var database = mongoClient.GetDatabase(databaseName);
+
+        var inventoryCollections = database.GetCollection<InventoryEntry>("InventoryEntries");
+        if(await inventoryCollections.EstimatedDocumentCountAsync() == 0)
+        {
+            await inventoryCollections.InsertManyAsync(GetPreconfiguredInventoryEntries());
+        }
+    }
+
+    private IEnumerable<InventoryEntry> GetPreconfiguredInventoryEntries()
+    {
+        return new List<InventoryEntry>()
+        {
+            new()
+            {
+                Quantity = 10,
+                DocumentNo = Guid.NewGuid().ToString(),
+                ItemNo = "PRD001",
+                ExternalDocumentNo = Guid.NewGuid().ToString(),
+                DocumentType = Shared.Enums.EDocumentType.Purchase,
+            },
+            new()
+            {
+                Quantity = 5,
+                DocumentNo = Guid.NewGuid().ToString(),
+                ItemNo = "PRD002",
+                ExternalDocumentNo = Guid.NewGuid().ToString(),
+                DocumentType = Shared.Enums.EDocumentType.Purchase,
+            }
+        };
+    }
+}
