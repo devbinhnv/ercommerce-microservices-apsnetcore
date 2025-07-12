ab6a61fa6b27751e6b17ec79d723ff2749e8ae70
devbinhnv <dev.binhnv@gmail.com>
2025-07-12 14:06:26 +0700

    feat: auto redirect to swagger

---
+++ src/Services/Inventory.Product.API/Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Product.API.Controllers;
+
+public class HomeController : ControllerBase
+{
+    public IActionResult Index() => Redirect("~/swagger");
+}
